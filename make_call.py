cad8c9c981ccd56600a35dc662def1374219f2cd
Jeremy Cornish-Ford <31107594+jcornishford@users.noreply.github.com>
2019-11-20 17:54:24 -0800

    Took out Security information
    Look on Twilio website for a different number, "from" number was cancelled.

--- make_call.py
+++ make_call.py
@@ -4,14 +4,14 @@ This program makes a call to a phone
 
 from twilio.rest import Client
 
-account_sid = "AC5b51ae547b4da4d350288f00a7269164"
-auth_token = "945a131166a7f79acf1a062ae1e8e702"
+account_sid = ""
+auth_token = ""
 client = Client(account_sid, auth_token)
 
 call = client.calls.create(	
 	url = 'http://demo.twilio.com/docs/voice.xml',
-	to = '5107073909',
-	from_= '2242314588',
+	to = '',
+	from_= '',
 	)
 
 print(call.sid)
